8d7555fb74819e7eb86784abac02b616bc67618b
axl84 <axl.faust@gmail.com>
2018-04-25 23:07:00 +0300

    links quickfix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="https://www.facebook.com/diandrunyk target="_blank"" className="icon fa-facebook-f"><span className="label">FB</span></a></li>
-                        <li><a href="https://www.instagram.com/diandrunyk/ target="_blank"" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                        <li><a href="https://www.facebook.com/diandrunyk" target="_blank" className="icon fa-facebook-f"><span className="label">FB</span></a></li>
+                        <li><a href="https://www.instagram.com/diandrunyk/" target="_blank" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
                         <li><a href="#" className="icon fa-behance"><span className="label">Behance</span></a></li>
-                        <li><a href="mailto:diandrunyk@gmail.com target="_top"" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="mailto:diandrunyk@gmail.com" target="_top" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Diana Andrunyk Photography</li><li>2018</li>
