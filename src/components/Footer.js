eac1410342a19999c36182b2241bfdf6720d7288
axl84 <axl.faust@gmail.com>
2018-04-26 00:57:33 +0300

    links + avatar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="https://www.facebook.com/diandrunyk" target="_blank" className="icon fa-facebook-f"><span className="label">FB</span></a></li>
                         <li><a href="https://www.instagram.com/diandrunyk/" target="_blank" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-                        <li><a href="#" className="icon fa-behance"><span className="label">Behance</span></a></li>
+                        <li><a href="https://www.behance.net/diandrunyk" target="_blank" className="icon fa-behance"><span className="label">Behance</span></a></li>
                         <li><a href="mailto:diandrunyk@gmail.com" target="_top" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
